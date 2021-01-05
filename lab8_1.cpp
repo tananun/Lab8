09a6a357bd7abf6aefc93a73366197a175d75ba6
tananun <76035726+tananun@users.noreply.github.com>
2021-01-05 17:24:01 +0700

    Update lab8_1.cpp

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 int main(){	
+	double loan,inr,pay;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> inr;
 	cout << "Enter amount you can pay per year: ";
-
+	cin >> pay;
+	double INR = loan*inr/100;
+	double tt = loan+INR;
+	double nb = tt-pay;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -20,14 +26,32 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	double preb = loan;
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	int i = 1;
+	while(nb >= 0){
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << preb;
+	cout << setw(13) << left << INR;
+	cout << setw(13) << left << tt;
+	cout << setw(13) << left << pay;
+	cout << setw(13) << left << nb;
+	preb = nb;
+	INR = preb*inr/100;
+	tt = preb + INR;
+	nb = tt-pay;
+	i++;
+	cout << "\n";
+	}
+	pay = tt;
+	nb = tt-pay;
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << preb;
+	cout << setw(13) << left << INR;
+	cout << setw(13) << left << tt;
+	cout << setw(13) << left << pay;
+	cout << setw(13) << left << nb;
+		
 	
 	return 0;
 }

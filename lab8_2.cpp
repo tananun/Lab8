77c64b69513836dc2dab74171bab52293a04f60b
tananun <76035726+tananun@users.noreply.github.com>
2021-01-05 17:24:31 +0700

    Update lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N,int M){
+if(N > 0 && M >0){
+	
+		for(int i=0;i<N;i++){
+			for(int j=0;j<M;j++){
+				cout << "O";
+	}
+		cout << "\n";		
+	}
+}
+else 
+	cout << "Invalid input";
+} 
+
 
 int main(){
 	
